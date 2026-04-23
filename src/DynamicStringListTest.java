3111fed35de54b880b019384c7d7c5ec01a6f8c1
WillyxWonka <kalif.edu@gmail.com>
2026-04-23 12:00:13 -0700

    initialized some J unit test

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class DynamicStringListTest {
-    
+    @Test
+    public void testConstructor(){
+
+        DynamicStringList list = new DynamicStringList();
+        assertEquals(list.size(), 0);
+    }
+        @Test
+    public void testConstructorWithInput(){
+
+        DynamicStringList list = new DynamicStringList(100);
+        assertEquals(list.capacity(), 100);
+    }
+
 }
