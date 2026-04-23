be51181e4dda0037e5b1d272cec8a08b0f3b4bea
WillyxWonka <kalif.edu@gmail.com>
2026-04-23 11:24:06 -0700

    initial commit

---
+++ src/DynamicStringList.java
@@ -0,0 +1,12 @@
+public class DynamicStringList implements StringList{
+
+    
+    String[] backingArray;
+
+    public DynamicStringList(int size){
+        backingArray = new String[size];
+    }
+
+    
+
+}
