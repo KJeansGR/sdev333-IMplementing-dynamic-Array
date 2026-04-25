a4013b78f7b3b331e5393f5807e9d8db7b0a43cc
TweakedStudios <tweaked.studios.contact@gmail.com>
2026-04-25 15:30:44 -0700

    finished test making PR

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -3,23 +3,32 @@ public class DynamicStringList implements StringList{
     
     String[] backingArray;
     int size = 0;
-    int capacity = 10;
+    int capacity = 10; // default capacity
 
-    public DynamicStringList(int size){
-        backingArray = new String[size];
-        capacity = size;
+    // Will create a new DynamicStringList with the given initial capacity.
+    public DynamicStringList(int initialCapacity){
+        backingArray = new String[initialCapacity];
+        capacity = initialCapacity;
     }
+
+    // Will create a new DynamicStringList with the default initial capacity of 10.
     public DynamicStringList(){
         backingArray = new String[capacity];
         
     }
-
+    
+    // Will get and return the string at the given index. 
+    // If the index is out of bounds, throw an IndexOutOfBoundsException.
+    @Override
     public String get(int index) {
         if (index >= size || index < 0) {
             throw new IndexOutOfBoundsException("index out of bounds");
         }
         return backingArray[index];
     }
+
+    // Will set the string at the given index to the given value.
+    @Override
     public void set(int index, String value){
         if (index >= size || index < 0) {
             throw new IndexOutOfBoundsException("index out of bounds");
@@ -27,6 +36,8 @@ public class DynamicStringList implements StringList{
         backingArray[index] = value;
     }
 
+    // Will add the given string to the end of the list.
+    @Override
     public void add(String word){
         if (word == null || word.length() == 0) {
             throw new IndexOutOfBoundsException("cannot be null or empty");
@@ -38,6 +49,9 @@ public class DynamicStringList implements StringList{
         size++;
 
     }
+    
+    //Will remove the string at the given index and return it.
+    @Override
     public String remove(int index){
         if (index >= size || index < 0) {
             throw new IndexOutOfBoundsException("index out of bounds");
@@ -46,11 +60,16 @@ public class DynamicStringList implements StringList{
         size --;
         return word;
     }
-      public int size(){
-        return size;
-      }
 
-      public int capacity(){
-        return capacity;
-      }
+    // Will return the number of strings currently in the list.
+    @Override
+    public int size(){
+    return size;
+    }
+
+    // Will return the current capacity of the list.
+    @Override
+    public int capacity(){
+    return capacity;
+    }
 }
--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,18 +1,20 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
 public class DynamicStringListTest {
     @Test
-    public void testConstructor(){
+    public void testConstructorInitialSizeisZero(){
 
         DynamicStringList list = new DynamicStringList();
         assertEquals(list.size(), 0);
     }
     @Test
-    public void testConstructorWithInput(){
+    public void testConstructorWithInputCapacity(){
 
         DynamicStringList list = new DynamicStringList(100);
+        
         assertEquals(list.capacity(), 100);
     }
     @Test
@@ -22,5 +24,66 @@ public class DynamicStringListTest {
         list.add("Strawberry");
         assertEquals(2, list.size());
     }
-
+    @Test
+    public void testGetReturnsCorrectValue() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        assertEquals("Apple", list.get(0));
+    }
+    @Test
+    public void testGetReturnsErrorWhenIndexOutOfBounds() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
+    }
+    @Test
+    public void testGetReturnsErrorWhenIndexOutOfBoundsNegetiveValue() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(-1));
+    }
+    @Test
+    public void testRemoveReturnsCorrectValue() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        list.add("Blueberry");
+        list.add("Snozberry");
+        assertEquals("Snozberry", list.get(3));
+    }
+    @Test
+    public void testRemoveReturnsErrorWhenIndexOutOfBounds() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        list.add("Blueberry");
+        list.add("Snozberry");
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
+    }
+    @Test
+    public void testRemoveReturnsErrorWhenIndexOutOfBoundsNegetiveValue() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Apple");
+        list.add("Strawberry");
+        list.add("Blueberry");
+        list.add("Snozberry");
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(-1));
+    }
+    @Test
+    public void testSizeReturnsCorrectValue() {
+        DynamicStringList list = new DynamicStringList(100);
+        list.add("Apple");
+        list.add("Strawberry");
+        list.add("Blueberry");
+        list.add("Snozberry");
+        assertEquals(4, list.size());
+    }
+    @Test
+    public void testCapacityReturnsCorrectValue() {
+        DynamicStringList list = new DynamicStringList(100);
+        assertEquals(100, list.capacity());
+    }
 }
